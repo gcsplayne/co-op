a52ed0cab62446fd9ca75c8b70a87a2601f75846
Matt <home@Matts-iMac.local>
2021-08-17 01:53:38 -0400

    betalivetest

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,29 @@
 import styled from 'styled-components'
+import Link from 'next/link';
+import Image from 'next/image'
 
 
 
 const Footer = () => {
     return (
        <footer>
-            <p>Website Created By Matt Playne</p>
+             
+            {/* <div className='footlogo'>
+            <p>Website Created By: </p> 
+          <Image src="/images/logo_black.png" width={60} height={60} alt="footerlogo"></Image>
+          <br/>Email: mattplayne@gmail.com
+        <br/>Phone: 705-937-1421
+        </div> */}
+        <div className='footlogo'>
+        <p>Website Created By: </p>
+        <Link href="/"><a className={'footlink'}>
+          <Image src="/images/logo_black.png" width={60} height={60} alt="footerlogo"></Image>
+          </a></Link> </div>
+         <div className='footcontact'>
+          <br/>Email: mattplayne@gmail.com
+        <br/>Phone: 705-937-1421
+        </div>
+       
             </footer>
     )
 }
